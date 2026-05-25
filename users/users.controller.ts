0fddc274e6d361113774381120014b6ee5bc0008
ahmedgf1977 <ahmed.garcia@zavixbrands.com>
2026-05-25 14:33:13 -0600

    Create users.controller.ts

---
+++ users/users.controller.ts
@@ -0,0 +1,20 @@
+import { Controller, Get, Patch, Param, Body, UseGuards } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { JwtAuthGuard } from '../auth/jwt-auth.guard';
+
+@Controller('users')
+export class UsersController {
+  constructor(private usersService: UsersService) {}
+
+  @Get()
+  @UseGuards(JwtAuthGuard)
+  findAll() {
+    return this.usersService.findAll();
+  }
+
+  @Patch(':id')
+  @UseGuards(JwtAuthGuard)
+  update(@Param('id') id: string, @Body() body: any) {
+    return this.usersService.update(Number(id), body);
+  }
+}
